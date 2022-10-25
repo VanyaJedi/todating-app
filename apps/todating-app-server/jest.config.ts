d3f3b20091f717ea2366f4a395e7e738fe895aed
Ivan Nekipelov <inekipelov@vivid.money>
2022-10-26 04:30:22 +0700

    init

--- apps/todating-app-server/jest.config.ts
+++ apps/todating-app-server/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'todating-app',
+  displayName: 'todating-app-server',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/todating-app',
+  coverageDirectory: '../../coverage/apps/todating-app-server',
 };
